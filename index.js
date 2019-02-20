18202ed116baf13e088c6e0bfb38dd37b9122c8b
Robin Tram <robin.tram@gmail.com>
2019-02-20 21:23:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ function findMinAndRemoveSorted(array) {
   array.shift()
   return min
 }
+
+function merge(first, second) {
+  let sortedArray = []
+  
+  while (first.length > 0 && second.length > 0) {
+    if (first[0] < second[0]) {
+      sortedArray.push(findMinAndRemoveSorted(first))
+    } else {
+      sortedArray.push(findMinAndRemoveSorted(second))
+    }
+  }
+  return sortedArray.concat
+}
